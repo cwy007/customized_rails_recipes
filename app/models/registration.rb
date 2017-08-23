d6318b0a4c9a4e433bfda935f517671858b62e83
chanweiyan <chanweiyan007@gmail.com>
2017-08-24 03:10:08 +0800

    self define validate and use flash.now to show errors[:base]

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,6 +8,8 @@ class Registration < ApplicationRecord
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
   validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
 
+  validate :check_event_status, :on => :create
+
   belongs_to :event
   belongs_to :ticket
   belongs_to :user, :optional => true # REVIEW
@@ -20,6 +22,12 @@ class Registration < ApplicationRecord
 
   protected
 
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开始报名")
+    end
+  end
+  
   def should_validate_basic_data?
     current_step == 2
   end
