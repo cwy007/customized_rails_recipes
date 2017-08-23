200870b1686ffacf9dc0c82c4a024c916bf78053
chanweiyan <chanweiyan007@gmail.com>
2017-08-24 01:40:18 +0800

    conditional validation and attr_accessor virtual field

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,6 +4,10 @@ class Registration < ApplicationRecord
   validates_inclusion_of :status, :in => STATUS
   validates_presence_of :status, :ticket_id
 
+  attr_accessor :current_step  # NOTE
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+
   belongs_to :event
   belongs_to :ticket
   belongs_to :user, :optional => true # REVIEW
@@ -16,8 +20,16 @@ class Registration < ApplicationRecord
 
   protected
 
+  def should_validate_basic_data?
+    current_step == 2
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed"
+  end
+
   def generate_uuid
     self.uuid = SecureRandom.uuid
-  end 
+  end
 
 end
